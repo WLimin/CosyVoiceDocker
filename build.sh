575e863ad5c7cdcfe4e68f8d10b2ce2623de0b32
wang <w.limin@qq.com>
2025-07-14 10:05:45 +0800

    更正构建用户参数

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@
 # 12.4.1-devel-ubuntu22.04 (12.4.1/ubuntu22.04/devel/Dockerfile)
 # 12.4.1-runtime-ubuntu22.04 (12.4.1/ubuntu22.04/runtime/Dockerfile)
 # auto,plain
-docker build -t cosyvoice-gpu --progress=plain -f docker/Dockerfile .
+docker build -t cosyvoice-gpu --progress=plain  --build-arg  ARG_USER_NAME=webui -f docker/Dockerfile .
 # docker run -d -it --name cosy-voice --network=openwebui-net -v ${PWD}/pretrained_models:/workspace/CosyVoice/pretrained_models  -v ${PWD}/asset:/workspace/CosyVoice/asset -p 8086:8080 -p 8087:8000 -e CUDA_ENABLED=false -e MODEL_PATH=pretrained_models/CosyVoice2-0.5B cosyvoice
 # curl -X POST "http://127.0.0.1:8087/v1/audio/speech"  -H "Content-Type: application/json"  -d '{ "input": "Hello, 中文和英文混合测试。this is a test of the MeloTTS API.", "voice": "中文女", "response_format": "wav","speed": 1.0 }' --output /tmp/nfs/output.wav
 
